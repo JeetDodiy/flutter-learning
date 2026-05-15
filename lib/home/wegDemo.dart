c4f6f666516ba28ca9ea32cae1dd568434f3b85a
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-15 18:00:14 +0530

    Create my on couter

--- lib/home/wegDemo.dart
+++ lib/home/wegDemo.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 
-class Wegdemo extends StatelessWidget{
+class Wegdemo extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _WegdemoSate();
+  }}
+  class _WegdemoSate extends State<StatefulWidget>{
+  var number = 0;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Number :$number",style: TextStyle(),),
+            ElevatedButton(onPressed: (){
+              setState(() {
+                number ++;
+
+              });
+
+            }, child: Text('Press'))
+          ],
+        ),
+      ),
+    );
   }
-}
+
+  }
+
 
 
