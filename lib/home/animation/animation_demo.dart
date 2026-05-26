2fe2cd6669a14731828e1de73ba7c5d1966e6bde
JeetDodiy <jeetdodiya660741@gmail.com>
2026-05-26 17:10:42 +0530

    change file logic

--- lib/home/animation/animation_demo.dart
+++ lib/home/animation/animation_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class AniCon extends StatefulWidget {
   const AniCon({super.key});
 
@@ -6,29 +7,42 @@ class AniCon extends StatefulWidget {
   State<AniCon> createState() => _AniConState();
 }
 
-class _AniConState extends State<AniCon> with SingleTickerProviderStateMixin{
+class _AniConState extends State<AniCon> with SingleTickerProviderStateMixin {
   late Animation animation;
   late Animation colorAnimation;
   late AnimationController animationController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    animationController = AnimationController(vsync: this,duration: Duration(seconds: 15));
-    animation = Tween(begin: 30 , end: 400.0).animate(animationController);
-    colorAnimation = ColorTween(begin: Colors.black87 , end: Colors.lime.shade300).animate(animationController);
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 15),
+    );
+    animation = Tween(begin: 30, end: 400.0).animate(animationController);
+    colorAnimation = ColorTween(
+      begin: Colors.black87,
+      end: Colors.lime.shade300,
+    ).animate(animationController);
     animationController.addListener(() {
       print(animation.value);
-      setState(() {
-
-      });
-    },);
+      setState(() {});
+    });
     animationController.forward();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Container(color: colorAnimation.value,width: animation.value,height: animation.value,)),
+      body: Center(
+        child: Container(
+          color: colorAnimation.value,
+          width: animation.value,
+          height: animation.value,
+          child: Icon(Icons.call, size: 35),
+        ),
+      ),
     );
   }
 }
